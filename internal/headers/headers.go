503c39731153d054b7aee20272454cde1c8cbedf
chappywagner <wagnermichaeljames@gmail.com>
2025-04-03 22:44:19 -0500

    corrected headers bug 9

--- internal/headers/headers.go
+++ internal/headers/headers.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Headers map[string]string 
-const CRLF = "\r\n\r\n"
+const CRLF = "\r\n"
 
 func NewHeaders() Headers{
 	return make( map[string]string)
@@ -18,49 +18,48 @@ func (h Headers) Parse(data [] byte) (n int, done bool, err error){
 	fmt.Printf("Parsing header data: %s...\r\n",string(data))
 	
 	nbytes := 0
-	for !done{
-		
-	if len(data)==0{
-		return 0, true, errors.New("no data")
+	crlfidx := bytes.Index(data,[]byte(CRLF))
+	
+	if crlfidx == 0{
+		return 2,true, nil
+	
 	}
+
+	endIdx := bytes.Index(data, []byte(CRLF))
+    if endIdx == -1 {
+        // Not enough data yet
+        return 0, false, nil
+    }
 
 	idx := bytes.Index(data,[]byte(":"))
 	fmt.Printf("  **** Index of : is %v\n",idx)
 	if idx == -1 || idx < 4{
-		return 0,false, errors.New("improperly formatted header")
+		return 0,false, errors.New("header missing colon separator")
 	}	
 	
 	if data[idx - 1] == ' '{
-		return 0,false, errors.New("improperly formatted header")
+		return 0,false, errors.New("space before colon separator not permitted")
 	}
 
-		//
-		endidx := bytes.Index(data,[]byte(CRLF))
-
-		if endidx == -1{
-			return 0, false, nil
-		}
+		
 
 
-		if endidx == 0{
-			done = true
-			break
-		}
+		
+		key := string(bytes.TrimSpace(data[:idx]))
+		val := string(bytes.TrimSpace(data[idx+1:endIdx]))
 
-
-		sepidx := bytes.Index(data,[]byte(": "))
-		key := string(bytes.Trim(data[:sepidx]," "))
-		val := string(bytes.Trim(data[sepidx+2:endidx]," "))
 		nbytes += len(key) + len(val)
 
 		h[key]=val
-		newdata:=make([]byte,len(data)-len(CRLF))
-		copy(newdata,data[endidx:])
-		data = newdata
+		//newdata:=make([]byte,len(data)-len(CRLF))
+		//copy(newdata,data[endidx:])
+		//data = newdata
+
 		fmt.Printf("new data value is %s\r\nbytes read: %v",string(data),nbytes)
 
+		return endIdx +2,false, nil
 	}
 
-	return nbytes,done,nil
+	
 
-}
+
